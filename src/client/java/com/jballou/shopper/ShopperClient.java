59015ddae9871f1a51c23df3b00e649e7b795f11
Lafolie <lafoliex7@aol.com>
2023-07-12 21:24:44 +0100

    added client initialiser skeleton

---
+++ src/client/java/com/jballou/shopper/ShopperClient.java
@@ -0,0 +1,18 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.loader.impl.util.log.Log;
+
+public class Shopper implements ClientModInitializer {
+
+	public static final String MOD_ID = "shopper";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitializeClient() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
