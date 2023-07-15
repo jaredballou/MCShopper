a32acf466d494baf7379f92a204faf2eb6dc1ce4
Lafolie <lafoliex7@aol.com>
2023-07-15 09:57:43 +0100

    added dummy function for getting stock

--- src/client/java/com/jballou/shopper/data/ShopSign.java
+++ src/client/java/com/jballou/shopper/data/ShopSign.java
@@ -18,6 +18,7 @@ public class ShopSign
 	public float buyPrice;
 	public float sellPrice;
 	public String itemName;
+	public int stock;
 
 	public ShopSign(JsonObject json, Identifier dimension)
 	{
@@ -27,6 +28,7 @@ public class ShopSign
 		buyPrice = json.get("buyPrice").getAsFloat();
 		sellPrice = json.get("sellPrice").getAsFloat();
 		itemName = json.get("itemName").getAsString();
+		stock = json.get("stock").getAsInt();
 
 		this.dimension = dimension;
 	}
@@ -42,6 +44,36 @@ public class ShopSign
 		this.buyPrice = buyPrice;
 		this.sellPrice = sellPrice;
 		itemName = text.getMessage(3, useFrontSide).getString();
+		stock = findStock(sign, itemName);
+	}
+
+	private int findStock(SignBlockEntity sign, String itemName)
+	{
+		// This function could be impossible to write.
+		// I have outlined the issues and strategy here
+
+		// find the item Identifer from the itemName
+		//		this is tricky
+		//		depends on how the shop plugin works
+		//		need to convert the item name string:
+		//			swap spaces for underscores
+		//			prefix a tag namespace
+		//			if there are custom blocks, this is an impossible task
+		//		use converted string to make an Identifer
+		// however, if the chests always contain 1 kind of item, we don't need an Identifier
+
+		// find the chest block
+		//		check each direction for a chest block
+		//		BlockPos has funcs like BlockPos.up(1) that makes this easy
+		//		Difficulty here is to know which chest is associated with the sign:
+		//			an Identifier would help alleviate this
+		//			if an Identifier is a solution, we can get the ID from the ItemStacks and strip out the path
+		//			replace stripped out path underscores/slashes with spaces. Compare with itemName
+		//			if itemName matches name derived from tag, we have found the chest:
+		//				Issue: does not work for adjacent shops selling the same items
+		// query the inventory, for each item stack add the count to an accumulator
+		// return the accumulator
+		return 0;
 	}
 
 	public float getBuyValue()
@@ -76,6 +108,7 @@ public class ShopSign
 		obj.addProperty("buyPrice", buyPrice);
 		obj.addProperty("sellPrice", sellPrice);
 		obj.addProperty("itemName", itemName);
+		obj.addProperty("stock", stock);
 
 		return obj;
 	}
